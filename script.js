6e4bd9e19b532d9d51fec23a210a36a7ebaf08ad
py1tgrd <54027673+py1tgrd@users.noreply.github.com>
2020-12-16 22:23:13 +0000

    Better touch screen handling

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 
 
-document.querySelector('.container').addEventListener("click",()=>{
-    document.querySelectorAll(".triangle").forEach((t)=>t.classList.toggle("activated"));
-    document.querySelector(".container").classList.toggle("activated");
-});
+const _C = document.querySelector('.container');
 
+
+const togglePointThuster = (e) =>{
+    e.preventDefault();
+    document.querySelectorAll(".triangle").forEach((t)=>t.classList.toggle("activated"));
+    _C.classList.toggle("activated");
+}
+
+
+_C.addEventListener("click",e=>togglePointThuster(e));
+_C.addEventListener("touchstart",e=>togglePointThuster(e));
+_C.addEventListener('touchmove', e => {e.preventDefault()}, false)
